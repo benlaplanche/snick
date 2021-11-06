f566171c30bd0694dbbe8e0c85d87fea9e836bc1
Ben Laplanche <ben@laplanche.co.uk>
2021-11-06 17:40:55 +0000

    added environment

---
+++ environment/environment_test.go
@@ -0,0 +1,12 @@
+package environment_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/benlaplanche/snick/environment"
+)
+
+var _ = Describe("Environment", func() {
+
+})
