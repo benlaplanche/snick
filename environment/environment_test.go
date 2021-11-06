50437cd032adf458d021fc98a7b8d805d2531fc8
Ben Laplanche <ben@laplanche.co.uk>
2021-11-06 18:37:31 +0000

    failing test

--- environment/environment_test.go
+++ environment/environment_test.go
@@ -6,8 +6,17 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Environment", func() {
-	result := DetectENV()
-	Expect(result).To(Equal("hello"))
+var _ = Describe("Environment Suite", func() {
+	Context("It is Github Actions", func() {
+
+		// os.Setenv("CI", "true")
+		// defer os.Unsetenv("CI")
+
+		// os.Setenv("GITHUB_ACTIONS", "true")
+		// defer os.Unsetenv(("GITHUB_ACTIONS"))
+
+		result := DetectENV()
+		Expect(result).To(Equal("GitHub Actions"))
+	})
 
 })
